ba81ec0a5f15df2fb0bd647f1b03b23ad92d28da
m1a1x12003 <125893848+m1a1x12003@users.noreply.github.com>
2025-09-25 18:10:28 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", () => {
+  console.log("✅ Service Worker installed");
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
